ad8337a9149e969ceb6d9d8db47e8fbc99b87ff3
AdamMatheny <adamamatheny@gmail.com>
2016-03-29 17:55:05 -0400

    -Added Remix level for Meme-theme

---
+++ Assets/Scripts/Environment/RandomObjectActivator.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class RandomObjectActivator : MonoBehaviour 
+{
+
+	[SerializeField] private GameObject[] mRandomObjects;
+	[SerializeField] private float mInitialWaitTime = 15f;
+	[SerializeField] private float mActiveTime = 30f;
+	[SerializeField] private float mInactiveTime = 30f;
+	[SerializeField] int mTargetRandomObject = 0;
+	// Use this for initialization
+	void Start () 
+	{
+		if(mRandomObjects.Length > 0)
+		{	
+			StartCoroutine(WaitAtStart());
+		}
+	}
+
+	IEnumerator WaitAtStart()
+	{
+		yield return new WaitForSeconds(mInitialWaitTime);
+		StartCoroutine(ActivateRandom());
+	}
+
+	IEnumerator ActivateRandom()
+	{
+		mTargetRandomObject = Random.Range(0,mRandomObjects.Length);
+		mRandomObjects[mTargetRandomObject].SetActive(true);
+		yield return new WaitForSeconds(mActiveTime);
+		StartCoroutine(DeactivateTarget());
+	}
+
+	IEnumerator DeactivateTarget()
+	{
+		mRandomObjects[mTargetRandomObject].SetActive(false);
+		yield return new WaitForSeconds(mInactiveTime);
+		StartCoroutine(ActivateRandom());
+	}
+}
